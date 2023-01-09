0304767886f3475dce85ff4868ef601860fe5359
gura-gura39 <113770480+gura-gura39@users.noreply.github.com>
2023-01-09 16:45:44 +0900

    HamburgerMenu Implementation

--- js/main.js
+++ js/main.js
@@ -1,8 +1,23 @@
 'use strict';
 
 {
-    // Intersection Observer API
+    const open = document.getElementById('open');
+    const overlay = document.querySelector('.overlay');
+    const close = document.getElementById('close');
 
+    open.addEventListener('click',()=>{
+        overlay.classList.add('show');
+        open.classList.add('hide')
+    }); 
+    
+    close.addEventListener('click',()=>{
+        overlay.classList.remove('show');
+        open.classList.remove('hide')
+    }); 
+
+
+
+    // Intersection Observer API
     function callback(entries, obs) {
         entries.forEach(entry => {
             if (!entry.isIntersecting) {
